4cc12bdc2302a0c352282ca8513d0f8c73c79a47
78109732 <ankanr596@gmail.com>
2025-10-07 19:54:16 +0530

    first commit

--- frontend/README.md
+++ frontend/README.md
@@ -10,3 +10,12 @@ Currently, two official plugins are available:
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+
+
+
+
+
+VITE_FIREBASE_APIKEY="AIzaSyDEPSXu3k-nWmbFETdE39cqYzQS96Zxjaw"
+VITE_GEOAPIKEY="6c76ef49f72d4455b0ab677cd63145cb"
+VITE_RAZORPAY_KEY_ID="rzp_test_RQWjBHRMtT37pD"
--- frontend/firebase.js
+++ frontend/firebase.js
@@ -7,11 +7,11 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey:import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "vingo-food-delivery-13b9e.firebaseapp.com",
-  projectId: "vingo-food-delivery-13b9e",
-  storageBucket: "vingo-food-delivery-13b9e.firebasestorage.app",
-  messagingSenderId: "237458808298",
-  appId: "1:237458808298:web:95d7934bfc6859c97d6693"
+  authDomain: "food-delivery-32ffa.firebaseapp.com",
+  projectId: "food-delivery-32ffa",
+  storageBucket: "food-delivery-32ffa.firebasestorage.app",
+  messagingSenderId: "663093314813",
+  appId: "1:663093314813:web:1aadd64e638445cabbea94"
 };
 
 // Initialize Firebase
@@ -23,22 +23,20 @@ export {app,auth}
 
 // // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
-// import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // // TODO: Add SDKs for Firebase products that you want to use
 // // https://firebase.google.com/docs/web/setup#available-libraries
 
 // // Your web app's Firebase configuration
 // const firebaseConfig = {
-//   apiKey:import.meta.env.VITE_FIREBASE_APIKEY,
-//   authDomain: "vingo-food-delivery-13b9e.firebaseapp.com",
-//   projectId: "vingo-food-delivery-13b9e",
-//   storageBucket: "vingo-food-delivery-13b9e.firebasestorage.app",
-//   messagingSenderId: "237458808298",
-//   appId: "1:237458808298:web:95d7934bfc6859c97d6693"
+//   apiKey: "AIzaSyDEPSXu3k-nWmbFETdE39cqYzQS96Zxjaw",
+//   authDomain: "food-delivery-32ffa.firebaseapp.com",
+//   projectId: "food-delivery-32ffa",
+//   storageBucket: "food-delivery-32ffa.firebasestorage.app",
+//   messagingSenderId: "663093314813",
+//   appId: "1:663093314813:web:1aadd64e638445cabbea94"
 // };
 
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 // const auth=getAuth(app)
-// const provider=new GoogleAuthProvider()
-// export {provider,auth}
+// export {app,auth}
