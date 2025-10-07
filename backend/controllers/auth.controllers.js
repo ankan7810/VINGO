7253652c4b7ef797aa5e5419f442b0ad76eac6fd
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2025-10-07 20:10:08 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -57,8 +57,8 @@ export const signUp = async (req, res) => {
 
     const token = await genToken(user._id);
     res.cookie("token", token, {
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
     });
@@ -84,8 +84,8 @@ export const signIn = async (req, res) => {
 
     const token = await genToken(user._id);
     res.cookie("token", token, {
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
     });
@@ -173,8 +173,8 @@ export const googleAuth = async (req, res) => {
 
     const token = await genToken(user._id);
     res.cookie("token", token, {
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
     });
